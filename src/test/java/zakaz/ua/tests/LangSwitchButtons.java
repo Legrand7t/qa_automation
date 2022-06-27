b270c62e689006fa08ce5402439bc68beb89292a
Anatoly Bakal <legrand7t@gmail.com>
2022-06-27 15:44:07 +0300

    lang_switch

---
+++ src/test/java/zakaz/ua/tests/LangSwitchButtons.java
@@ -0,0 +1,20 @@
+package zakaz.ua.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import zakaz.ua.pageObjects.ZakazHomePage;
+
+public class LangSwitchButtons extends TestInit{
+
+    @Test
+    public void doIt() {
+
+        getZakaz();
+        implicitlyWait();
+
+        ZakazHomePage homePage = new ZakazHomePage(driver);
+
+        // перевірка відображення всіх кнопок для вибору мови
+        Assert.assertTrue(homePage.getLangSwitchButtons().size() == 3);
+    }
+}
